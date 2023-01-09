e1f86c1ad8e8580e201f5adc800f8e1a39a41f2f
VijaykumarBhavanur <vijaykumarbhavanur@gmail.com>
2023-01-09 08:51:32 +0530

    Added: Odd Even Program

---
+++ src/com/java/program/OddEven.java
@@ -0,0 +1,15 @@
+package com.java.program;
+
+public class OddEven {
+
+	public static void main(String[] args) {
+		int num=91;
+		
+		if(num%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+
+	}
+
+}
